15903108e04b1b182b7bc64cd577a5a0931bec28
tank0412 <romabukhtiarov@gmail.com>
2018-02-17 13:08:32 +0300

    gtaxlwifi: update cpu config from exynos7870 org

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,7 +39,7 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_VARIANT := generic
 TARGET_CPU_CORTEX_A53 := true
 
 # Binder
@@ -187,6 +187,8 @@ endif
 
 # SELinux
 BOARD_SEPOLICY_DIRS := $(LOCAL_PATH)/sepolicy
+
+ALLOW_MISSING_DEPENDENCIES=true
 
 # Inherit from the proprietary version
 -include vendor/samsung/gtaxlwifi/BoardConfigVendor.mk
